52e267db7555246a2142b8e10c0b432cf45019ee
ISBAKHUL LAIL <91885498+isbakhul21@users.noreply.github.com>
2024-07-23 08:04:32 +0700

    update all

--- models/thesis_guidance.py
+++ models/thesis_guidance.py
@@ -9,13 +9,15 @@ class ThesisGuidance(models.Model):
 
     student_id = fields.Many2one("academy.student", string="Name")
     guidance_date = fields.Date(string="Guidance Date", default=fields.Date.context_today)
-    accept_date =  fields.Datetime(string="Accept Date", default=fields.Datetime.now)
+    accept_date = fields.Datetime(string="Accept Date", default=fields.Datetime.now)
     notes = fields.Html(string="Notes")
     date_consultation = fields.Date(string="Date Consultation")
     date_proposal = fields.Date(string="Date Proposal")
     date_test_thesis = fields.Date(string="Date Test Thesis")
     date_graduate = fields.Date(string="Date Graduate")
 
+    # LineGuidance
+    thesis_guidance_line_ids = fields.One2many('thesis.guidance.line','thesis_guidance_id',string="Thesis Guidance Line")
 
 
     priority = fields.Selection([
@@ -65,6 +67,59 @@ class ThesisGuidance(models.Model):
     def action_graduate(self):
         for this in self:
             this.state = 'graduate'
+
+
+class ThesisGuidanceLine(models.Model):
+    _name = "thesis.guidance.line"
+    _inherit = ["mail.thread","mail.activity.mixin"]
+    _description = 'Thesis Guidance Line'
+
+    thesis_guidance_id = fields.Many2one('thesis.guidance',string="Thesis Guidance")
+
+    subject = fields.Char(string="Mata Kuliah")
+    lecturer = fields.Char(string="Dosen")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class HewanOrm(models.Model):
+    _name = "hewan.orm"
+    _inherit = ["mail.thread","mail.activity.mixin"]
+    _description = 'Hewan Orm'
+
+    nama = fields.Char(string="Nama")
+    umur = fields.Integer(string="Umur")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
